eb095bab3553c94269cb37f02854041eb438acad
nggar <enggarjpx@gmail.com>
2022-07-01 17:48:32 +0700

    import Thicccboi font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+// Fonts
+import './fonts/TTF/THICCCBOI-Regular.ttf';
+
+// import styles
+import './styles/app.scss';
 
 function App() {
     return (
         <div className="App">
-
+            <h1>bold</h1>
+            <p className='semi-bold'>semi bold</p>
+            <p className='normal'>normal</p>
         </div>
     );
 }
