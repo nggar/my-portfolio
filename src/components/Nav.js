504fa194faef08810571d9fb1b6f8c215c9e80cc
nggar <enggarjpx@gmail.com>
2022-07-04 23:43:54 +0700

    create nav-menu on mobile device

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
+
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState( false );
+    const navBtnHandler = () => {
+        setActiveNav( !activeNav );
+    }
+
     return (
         <div className="main-container">
             <div className="container">
-                <div className="container">
-                    <div className="nav section-margin">
+                <div className="nav section-margin">
 
-                        <div className="nav__logo">
-                            <h1 className='logo-text'>Portfolio</h1>
-                        </div>
-                        <div className="nav__links">
-                            <a className='link-nav' href="#">About</a>
-                            <a className='link-nav' href="#">Contact</a>
-                        </div>
+                    <div className="nav__logo">
+                        <h1 className='logo-text'>Portfolio</h1>
+                    </div>
+                    <div className={`nav__links ${activeNav ? 'nav__links--mobile' : ''}`}>
+                        <a className={`link-nav ${activeNav ? 'link-nav--white' : ''}`} href="#">About</a>
+                        <a className={`link-nav ${activeNav ? 'link-nav--white' : ''}`} href="#">Projects</a>
+                        <a className={`link-nav ${activeNav ? 'link-nav--white' : ''}`} href="#">Contact</a>
+                    </div>
+                    <div onClick={navBtnHandler} className={`nav-outside ${activeNav ? 'nav-outside--active' : ''}`}></div>
+
+                    <div onClick={navBtnHandler} className={`nav__btn ${activeNav ? 'nav__btn--active' : ''}`}>
+                        <div className="nav__btn__icon nav__btn__icon--1"></div>
+                        <div className="nav__btn__icon nav__btn__icon--2"></div>
                     </div>
                 </div>
             </div>
