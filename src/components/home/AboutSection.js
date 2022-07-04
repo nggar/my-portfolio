3108cf648934d99231c184ba5e075c12ad2714fc
nggar <enggarjpx@gmail.com>
2022-07-04 21:19:48 +0700

    change projects section layout

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -16,7 +16,7 @@ const AboutSection = () => {
                         </div>
                         <div className="about-profile__link">
                             <a href="" className="link-arrow">
-                                <div className="link-arrow__text">Browse portfolio</div>
+                                <div className="link-arrow__text">More about me</div>
                                 <div className="link-arrow__icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -5,52 +5,70 @@ import project3 from '../../assets/images/frame-3.png';
 const Projects = () => {
     return (
         <div className="main-container">
-            <div className="container container__project-section">
-                <div className="projects-section section-padding">
-                    <h1 className="projects-heading heading-2">Browse my portfolio</h1>
+            <div className="container">
+                <div className="projects-section section-padding section-margin border-bottom">
+                    <h1 className="projects-heading heading-2">Recent projects</h1>
                     <div className="projects-wrapper">
-                        <div className="project">
-                            <div className="project__img">
-                                <img src={project1} alt="project-1" />
-                            </div>
-                            <div className="project__description border-bottom">
-                                <div className="project-info">
-                                    <h4 className="project-name">Sezane</h4>
-                                    <div className="project-info__line"></div>
-                                    <h4 className="project-topic">Branding</h4>
+                        <a className='project--1' href="#">
+                            <div className="project">
+                                <div className="project__img">
+                                    <img src={project1} alt="project-1" />
                                 </div>
-                                <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere neque vel est unde odio maiores, aspernatur possimus.</p>
-                            </div>
-                        </div>
-                        <div className="project">
-                            <div className="project__img">
-                                <img src={project2} alt="project-1" />
-                            </div>
-                            <div className="project__description border-bottom">
-                                <div className="project-info">
-                                    <h4 className="project-name">Doen</h4>
-                                    <div className="project-info__line"></div>
-                                    <h4 className="project-topic">Logo Design</h4>
+                                <div className="project__description">
+                                    <div className="project-info">
+                                        <h4 className="project-name">Sezane</h4>
+                                        <div className="project-info__line"></div>
+                                        <h4 className="project-topic">Branding</h4>
+                                    </div>
+                                    <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
                                 </div>
-                                <h3 className="sub-heading project-title">Branding & logo design for Doen</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere neque vel est unde odio maiores, aspernatur possimus.</p>
                             </div>
-                        </div>
-                        <div className="project">
-                            <div className="project__img">
-                                <img src={project3} alt="project-1" />
-                            </div>
-                            <div className="project__description">
-                                <div className="project-info">
-                                    <h4 className="project-name">La Ligne</h4>
-                                    <div className="project-info__line"></div>
-                                    <h4 className="project-topic">Web Design</h4>
+                        </a>
+                        <a className='project--2' href="#">
+                            <div className="project">
+                                <div className="project__img">
+                                    <img src={project2} alt="project-1" />
                                 </div>
-                                <h3 className="sub-heading project-title">Brand design & identity for La Ligne</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere neque vel est unde odio maiores, aspernatur possimus.</p>
+                                <div className="project__description">
+                                    <div className="project-info">
+                                        <h4 className="project-name">Doen</h4>
+                                        <div className="project-info__line"></div>
+                                        <h4 className="project-topic">Logo Design</h4>
+                                    </div>
+                                    <h3 className="sub-heading project-title">Branding & logo design for Doen</h3>
+                                </div>
                             </div>
-                        </div>
+                        </a>
+                        <a className='project--3' href="#">
+                            <div className="project">
+                                <div className="project__img">
+                                    <img src={project3} alt="project-1" />
+                                </div>
+                                <div className="project__description">
+                                    <div className="project-info">
+                                        <h4 className="project-name">La Ligne</h4>
+                                        <div className="project-info__line"></div>
+                                        <h4 className="project-topic">Web Design</h4>
+                                    </div>
+                                    <h3 className="sub-heading project-title">Brand design & identity for La Ligne</h3>
+                                </div>
+                            </div>
+                        </a>
+                        <a className='project--4' href="#">
+                            <div className="project">
+                                <div className="project__img">
+                                    <img src={project3} alt="project-1" />
+                                </div>
+                                <div className="project__description">
+                                    <div className="project-info">
+                                        <h4 className="project-name">La Ligne</h4>
+                                        <div className="project-info__line"></div>
+                                        <h4 className="project-topic">Web Design</h4>
+                                    </div>
+                                    <h3 className="sub-heading project-title">Brand design & identity for La Ligne</h3>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
