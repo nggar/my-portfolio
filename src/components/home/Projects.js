27c84779c9785fe4e27ae059ef0e1a74904d1845
nggar <enggarjpx@gmail.com>
2022-07-06 01:54:33 +0700

    make homepage sections responsive

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -9,69 +9,82 @@ const Projects = () => {
                 <div className="projects-section section-padding section-margin border-bottom">
                     <h1 className="projects-heading heading-2">Recent projects</h1>
                     <div className="projects-wrapper">
-                        <a className='project--1' href="#">
-                            <div className="project">
-                                <div className="project__img">
-                                    <img src={project1} alt="project-1" />
+
+                        {/* project 1 */}
+                        <a className="project" href="#">
+                            <div className="project__description">
+                                <div className="project-info">
+                                    <h4 className="project-name">Sezane</h4>
+                                    <div className="project-info__line"></div>
+                                    <h4 className="project-topic">Branding</h4>
                                 </div>
-                                <div className="project__description">
-                                    <div className="project-info">
-                                        <h4 className="project-name">Sezane</h4>
-                                        <div className="project-info__line"></div>
-                                        <h4 className="project-topic">Branding</h4>
+                                <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos similique facere cumque ipsa!</p>
+                                <a className='project__link' href='#'>
+                                    <div className="project__link__text">View project</div>
+                                    <div className="project__link__icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                        </svg>
                                     </div>
-                                    <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
-                                </div>
+                                </a>
+                            </div>
+                            <div className="project__img">
+                                <img src={project2} alt="project-1" />
                             </div>
                         </a>
-                        <a className='project--2' href="#">
-                            <div className="project">
-                                <div className="project__img">
-                                    <img src={project2} alt="project-1" />
+
+                        {/* project-2 */}
+                        <a className="project" href="#">
+                            <div className="project__description">
+                                <div className="project-info">
+                                    <h4 className="project-name">Sezane</h4>
+                                    <div className="project-info__line"></div>
+                                    <h4 className="project-topic">Branding</h4>
                                 </div>
-                                <div className="project__description">
-                                    <div className="project-info">
-                                        <h4 className="project-name">Doen</h4>
-                                        <div className="project-info__line"></div>
-                                        <h4 className="project-topic">Logo Design</h4>
+                                <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos similique facere cumque ipsa!</p>
+                                <a className='project__link' href='#'>
+                                    <div className="project__link__text">View project</div>
+                                    <div className="project__link__icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                        </svg>
                                     </div>
-                                    <h3 className="sub-heading project-title">Branding & logo design for Doen</h3>
-                                </div>
+                                </a>
+                            </div>
+                            <div className="project__img">
+                                <img src={project1} alt="project-1" />
                             </div>
                         </a>
-                        <a className='project--3' href="#">
-                            <div className="project">
-                                <div className="project__img">
-                                    <img src={project3} alt="project-1" />
+
+                        {/* project-3 */}
+                        <a className="project" href="#">
+                            <div className="project__description">
+                                <div className="project-info">
+                                    <h4 className="project-name">Sezane</h4>
+                                    <div className="project-info__line"></div>
+                                    <h4 className="project-topic">Branding</h4>
                                 </div>
-                                <div className="project__description">
-                                    <div className="project-info">
-                                        <h4 className="project-name">La Ligne</h4>
-                                        <div className="project-info__line"></div>
-                                        <h4 className="project-topic">Web Design</h4>
+                                <h3 className="sub-heading project-title">Brand design & identity for Sezane</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos similique facere cumque ipsa!</p>
+                                <a className='project__link' href='#'>
+                                    <div className="project__link__text">View project</div>
+                                    <div className="project__link__icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                        </svg>
                                     </div>
-                                    <h3 className="sub-heading project-title">Brand design & identity for La Ligne</h3>
-                                </div>
+                                </a>
                             </div>
-                        </a>
-                        <a className='project--4' href="#">
-                            <div className="project">
-                                <div className="project__img">
-                                    <img src={project3} alt="project-1" />
-                                </div>
-                                <div className="project__description">
-                                    <div className="project-info">
-                                        <h4 className="project-name">La Ligne</h4>
-                                        <div className="project-info__line"></div>
-                                        <h4 className="project-topic">Web Design</h4>
-                                    </div>
-                                    <h3 className="sub-heading project-title">Brand design & identity for La Ligne</h3>
-                                </div>
+                            <div className="project__img">
+                                <img src={project1} alt="project-1" />
                             </div>
                         </a>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
